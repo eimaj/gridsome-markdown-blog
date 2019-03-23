5419284740020db22292f146b3b4f583f7220bad
Jamie Allen <jamie@llen.ca>
2019-03-23 13:59:33 -0400

    Updates Default.vue w/ semantic html, components

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,80 +1,70 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <g-link class="heading-link" :to="{ name: 'home' }">
-        <component :is="titleTag" class="heading">{{ config.siteName }}</component>
-      </g-link>
-    </header>
-    <slot/>
+  <div class="Layout">
+    <Header />
+
+    <main class="Layout__main">
+      <slot />
+    </main>
+
+    <footer class="Layout__footer">
+      This is the footer
+    </footer>
   </div>
 </template>
 
 <script>
-import config from '~/.temp/config.js';
+import config from "~/.temp/config.js";
+import Header from "~/components/Header.vue";
 
 export default {
+  components: {
+    Header
+  },
+
   computed: {
-    titleTag () {
-      return this.$route.name === 'home' ? 'h1' : 'h6'
+    titleTag() {
+      return this.$route.name === "home" ? "h1" : "h6";
     },
-    config () {
-      return config
+    config() {
+      return config;
     }
   }
-}
+};
 </script>
 
 
 <style lang="css">
-  * {
-    margin: 0;
-    padding: 0;
-  }
+* {
+  margin: 0;
+  padding: 0;
+}
 
-  a {
-    color: #4dba87;
-    text-decoration: none;
-  }
+a {
+}
 
-  h1, h2, h3, h4, h5, h6 {
-    font-family: 'Mali', cursive;
-  }
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+}
 
-  h1 {
-    font-size: 48px;
-  }
+body {
+  font-size: 16px;
+}
 
-  h6 {
-    font-size: 20px;
-  }
+img {
+  max-width: 100%;
+  margin: 0 0 20px;
+}
+</style>
 
-  body {
-    font-family: 'Nunito', sans-serif;
-    margin: 0;
-    font-size: 16px;
-    padding: 0;
-  }
-
-  .heading-link {
-    color: black;
-    text-decoration: none;
-  }
-
-  .layout {
-    max-width: 600px;
-    margin: 0 auto;
-    padding-left: 20px;
-    padding-right: 20px;
-  }
-
-  .layout {
-    margin-top: 30px;
-    margin-bottom: 50px;
-  }
-
-  .heading {
-    margin-bottom: 20px;
-    font-family: 'Mali', cursive;
-    font-weight: bold;
-  }
+<style lang="css" scoped>
+.Layout {
+}
+.Layout__main {
+}
+.Layout__footer {
+}
 </style>
