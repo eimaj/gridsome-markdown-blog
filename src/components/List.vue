d8dbfb3ea0fd7f264d13fec521c8cecfb6e2f307
Jamie Allen <jamie@llen.ca>
2019-03-23 14:00:10 -0400

    Creates List.vue component

---
+++ src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="List">
+    <ListItem v-for="{ node } in posts" :key="node._id" :post="node" />
+  </section>
+</template>
+
+<script>
+import ListItem from "~/components/ListItem.vue";
+
+export default {
+  components: {
+    ListItem
+  },
+
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.List {
+}
+</style>
