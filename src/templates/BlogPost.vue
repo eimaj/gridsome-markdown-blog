6926227bc34c7aa102a1b70e2d7629449a8f4650
Jamie Allen <jamie@llen.ca>
2019-03-23 14:00:35 -0400

    Updates BlogPost.vue w/ semantic html, components

--- src/templates/BlogPost.vue
+++ src/templates/BlogPost.vue
@@ -1,22 +1,27 @@
 <template>
   <Layout>
-    <div class="article">
-      <h1>{{ $page.blogPost.title }}</h1>
-      <span>{{ $page.blogPost.date }}</span>
-      <g-image :src="$page.blogPost.image"/>
-      <div class="content" v-html="$page.blogPost.content" />
-    </div>
+    <article class="BlogPost">
+      <header class="BlogPost__header">
+        <h1 class="BlogPost__title">{{ $page.blogPost.title }}</h1>
+      </header>
+
+      <time :datetime="$page.blogPost.date" class="BlogPost__published">
+        {{ $page.blogPost.date }}
+      </time>
+
+      <section class="BlogPost__content" v-html="$page.blogPost.content" />
+    </article>
   </Layout>
 </template>
 
 <script>
 export default {
-  metaInfo () {
+  metaInfo() {
     return {
       title: this.$page.blogPost.title
-    }
+    };
   }
-}
+};
 </script>
 
 <page-query>
@@ -30,54 +35,13 @@ export default {
   }
 </page-query>
 
-<style>
-  /* .header {
-    margin-bottom: 70px;
-  } */
-
-  .article {
-    margin-top: 15px;
-  }
-
-  .article h1 {
-    font-size: 40px;
-  }
-
-  .article img {
-    width: 100%;
-    border-radius: 5px;
-  }
-
-  .article a {
-    color: #4dba87;
-    text-decoration: underline;
-  }
-
-  .article a:hover {
-    text-decoration: none;
-  }
-
-  .article span {
-    font-size: 80%;
-    margin-bottom: 20px;
-  }
-
-  .article ol, .article ul {
-    list-style-position: outside;
-  }
-
-  .article ul {
-    list-style: disc;
-    padding-left: 20px;
-  }
-
-  .article .content p:first-child {
-    margin-top: 15px;
-  }
-
-  .article .content p {
-    margin-top: 0;
-    margin-bottom: 10px;
-    line-height: 1.5;
-  }
+<style lang="css" scoped>
+.BlogPost__header {
+}
+.BlogPost__title {
+}
+.BlogPost__published {
+}
+.BlogPost__content {
+}
 </style>
