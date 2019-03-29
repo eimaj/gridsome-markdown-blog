76619e8aaf088d80704eee6af79c97e881267ea4
Jamie Allen <jamie@llen.ca>
2019-03-28 22:51:17 -0400

    Import and use the highlight directive in main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import DefaultLayout from '~/layouts/Default.vue';
-import Highlight from 'vue-markdown-highlight';
 import VueLocalStorage from 'vue-localstorage';
+import HighlightDirective from './directives/highlight';
 
-import 'highlight.js/styles/atom-one-dark.css';
 import './assets/styles/colors.scss';
 
 export default function(Vue, { head }) {
   Vue.component('Layout', DefaultLayout);
 
-  Vue.use(Highlight);
+  Vue.use(HighlightDirective);
   Vue.use(VueLocalStorage);
 
   head.link.push({
