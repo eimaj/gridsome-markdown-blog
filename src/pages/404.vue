5f4863f684cd02e2a8e5cbfe6477fe342af0f9d9
Jamie Allen <jamie@llen.ca>
2019-03-28 23:01:19 -0400

    Adds some styles to the 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,3 +1,37 @@
 <template functional>
-  <h1>404: Not found</h1>
+  <Layout>
+    <section class="FourOhFour">
+      <header class="FourOhFour__title">
+        <h1>404: Not found</h1>
+      </header>
+
+      <article class="FourOhFour__content">
+        <p>
+          This is probably not where you want to be...
+        </p>
+
+        <p>
+          <g-link
+            class="FourOhFour__home"
+            :to="{ name: 'home' }"
+            alt="Return to home page"
+            title="Return to home page"
+          >
+            Click here to return home
+          </g-link>
+        </p>
+      </article>
+    </section>
+  </Layout>
 </template>
+
+<style lang="scss" scoped>
+.FourOhFour {
+  border-bottom: 1px dashed var(--medium);
+  padding: 30px 0;
+}
+
+.FourOhFour__content {
+  padding-top: 20px;
+}
+</style>
